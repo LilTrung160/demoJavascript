2f370c2041e97dbe6da89141efd0668fbe2620bf
LilTrung160 <nguyenthanhtrung6999@gmail.com>
2024-03-13 19:35:48 +0700

    Thực hành: Hiển thị đầu ra

---
+++ hienthidaura.js
@@ -0,0 +1,15 @@
+function Message() {
+    document.getElementById('btn_message').innerHTML = "Hello World";
+}
+
+/*function Message() {
+    alert("Hello World");
+}*/
+
+/*function Message(){
+    document.write("Hello World");
+}*/
+
+/*function Message() {
+    console.log("Hello World");
+}*/
